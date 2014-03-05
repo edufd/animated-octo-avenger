c3c70a34935a4e8e413f62baaebc92b308d2113e
Eduardo <edudesvars@gmail.com>
2014-03-05 10:56:44 -0300

    Updated the views with template

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,13 @@
+from django.template import RequestContext
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hello world! <a href='/rango/about'>About</a>")
+    context = RequestContext(request)
+    
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
     return HttpResponse("Rango says: Here is the about page. <a href='/rango'>Index</a>")
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
@@ -26,6 +25,11 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
+TEMPLATE_DIRS = (
+    TEMPLATE_PATH,
+)
 
 # Application definition
 
@@ -80,4 +84,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
