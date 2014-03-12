3810f7066b673b2c4d665c116dc6373e224ccec6
Eduardo <edudesvars@gmail.com>
2014-03-11 23:07:48 -0300

    Added models and population script v2

--- rango/views.py
+++ rango/views.py
@@ -7,7 +7,13 @@ def index(request):
     context = RequestContext(request)
     
     context_dict = {'boldmessage': "I am bold font from the context"}
+    
     return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
-    return HttpResponse("Rango says: Here is the about page. <a href='/rango'>Index</a>")
+    context = RequestContext(request)
+    
+    context_dict = {'boldmessage': "I am bold font form the context"}
+    
+    return render_to_response('rango/about.html', context_dict, context)
+    #return HttpResponse("Rango says: Here is the about page. <a href='/rango'>Index</a>")
