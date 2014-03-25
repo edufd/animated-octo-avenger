6cca9acb30b3e97f59d5421e9111ea6e0c63930c
Eduardo <edudesvars@gmail.com>
2014-03-25 12:42:38 -0400

    Added the category form for creating a new category

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 # Create your views here.
 def index(request):
     # Obtain the context from the HTTP request.
@@ -70,4 +71,30 @@ def category(request, category_name_url):
         
     # Go render the response and return it to the client.    
     return render_to_response('rango/category.html', context_dict, context)
-
+    
+def add_category(request):
+    # Get the context from the request.
+    context = RequestContext(request)
+    
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        
+        # Have we been provided with a vaild form?
+        if form.is_valid():
+            # Save the new category to the database
+            form.save(commit=True)
+            
+            # Now call index() view.
+            # The user will be shown the homepage
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # if the request was not a POST, display the form to enter details.
+        form = CategoryForm()
+        
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render_to_response('rango/add_category.html', {'form': form}, context)
